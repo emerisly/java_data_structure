a6cf1ae59ce53490692de3d931804ac384d05457
emerisly <emeralderisly@gmail.com>
2021-03-28 01:50:04 -0400

    upload new codes

---
+++ rmit/src/V5/Book.java
@@ -0,0 +1,27 @@
+package V5;
+
+//define Book object
+public class Book {
+	
+	//field
+	String title;
+	String author;
+	int copy;
+	boolean eBook;
+	
+	
+	//constructor
+	public Book(String t,String a,int c,boolean p) {
+		title=t;
+		author=a;
+		copy=c;
+		eBook=p;
+	}
+	
+	//toString methods
+	public String toString() {
+		String s = eBook ? "ebook available":"no ebook";
+		return title+", "+ copy +" copies, "+ s;
+	}
+	
+}
---
+++ rmit/src/V5/BookStore.java
@@ -0,0 +1,213 @@
+package V5;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BookStore {
+	
+	public static Book[] bookList = new Book[5];
+    public static double totalPrice = 0;
+    public static int[] bookInCart = new int[1000];
+    public static int[] isPhysical = new int[1000];
+    public static int count = 1;
+	
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+		
+		Book book0 = new Book("Absolute Java","Savitch",5,true);
+		bookList[0]=book0;
+		Book book1 = new Book("JAVA: How to Program","Deitel and Deitel",0,true);
+		bookList[1]=book1;
+		Book book2 = new Book("Computing Concepts with JAVA 8 Essentials","Horstman",5,false);
+		bookList[2]=book2;
+		Book book3 = new Book("Java Software Solutions","Lewis and Loftus",5,false);
+		bookList[3]=book3;
+		Book book4 = new Book("Java Program Design","Cohoon and Davidson",1,true);
+		bookList[4]=book4;
+		
+        System.out.println("Welcome to Daintree!");
+        int input = 1;
+        
+        while(input != 0) {
+        	System.out.println();
+        	System.out.println("Choose an option:");
+        	System.out.println("1. Add a book to shopping cart");
+        	System.out.println("2. View shopping cart");
+        	System.out.println("3. Remove a book from shopping cart");
+        	System.out.println("4. Checkout");
+        	System.out.println("5. List all books");
+        	System.out.println("0. Quit");
+        	System.out.println("Please make a selection: ");
+        
+        	int option;
+        	option = myScanner.nextInt();
+        
+        	if (option == 0) input = 0;
+        	else if (option == 1) addBook();
+        	else if (option == 2) viewCart();
+        	else if (option == 3) removeBook();
+        	else if (option == 4) checkout();
+        	else if (option == 5) listBook();
+        	
+        	else System.out.println("Sorry, that is an invalid option!");
+        }
+        
+        System.out.println("Goodbye!");
+        
+        
+    }
+
+	public static int addBook() {
+		System.out.println("Enter title to search for: ");
+		Scanner name = new Scanner(System.in);
+		String bookName = name.nextLine();
+		String newBookName = bookName.toLowerCase();
+		int cnt = 0;
+		int i=0;
+		int[] matchedBook = new int[5];
+		boolean firstTime = true;
+		for(i=0;i<=4;i++) {
+			String newBookList = bookList[i].title.toLowerCase();
+			if(newBookList.startsWith(newBookName)) {
+				if(firstTime) System.out.println("The Following Title is a match:");
+				firstTime = false;
+				System.out.println((cnt+1)+". " + bookList[i].title + " -- " +bookList[i].author);
+				matchedBook[cnt]=i;
+				cnt++;
+			}
+		}
+		if(cnt!=0) {
+				System.out.println("0. cancel");
+				System.out.println("What is your selection:");
+				Scanner myScanner = new Scanner(System.in);
+				int selection1 = myScanner.nextInt();
+				//if choose to buy book
+				if (selection1 >= 1 && selection1 <= cnt) {
+					System.out.println("Do you want to buy this as an ebook: ");
+					String selection2 = myScanner.next();
+					//if choose to buy ebook
+					if (selection2.toLowerCase().equals("yes")) {
+						//if has ebook
+						if(bookList[matchedBook[selection1-1]].eBook) {
+							System.out.println("\""+bookList[matchedBook[selection1-1]].title+"\" has been added to your Cart");
+							bookInCart[count] = matchedBook[selection1-1];
+							totalPrice += 8;
+							isPhysical[count] = 1;
+							count++;
+						}
+						//if no ebook
+						else {
+							System.out.println("There are no eBook of that book available!");
+						}
+					}
+					//if choose to buy physical
+					else if(selection2.toLowerCase().equals("no")) {
+						//if has copies
+						if(bookList[matchedBook[selection1-1]].copy>0) {
+							System.out.println("\""+bookList[matchedBook[selection1-1]].title+"\" has been added to your Cart");
+							bookInCart[count] = matchedBook[selection1-1];
+							totalPrice += 50;
+							isPhysical[count] = 2;
+							count++;
+						}
+						//if has no copies
+						else {
+							System.out.println("There are no physical copies of that book available!");
+						}
+					}
+					else {
+						System.out.println("Sorry, that is an invalid option!");
+					}
+				}
+				//if choose cancel
+				else if(selection1 == 0) {
+					return 0;
+				}
+				//invalid
+				else {
+					System.out.println("Sorry, that is an invalid option!");
+				}
+		}
+		if(cnt==0) System.out.println("There is no title starting with that");
+		return 0;
+	}
+	
+	public static int viewCart() {
+		if (count<=1) {
+			System.out.println("There is no book in your cart.");
+		}
+		else {
+			System.out.println("Shopping Cart contains the following:");
+			for(int i=1;i<count;i++) {
+				System.out.println(i+". " + bookList[bookInCart[i]].title + " -- " + bookList[bookInCart[i]].author);
+			}
+		}
+		return 0;
+		
+	}
+	
+	public static int removeBook() {
+		if (count<=1) {
+			System.out.println("There is no book in your cart.");
+		}
+		else {
+			System.out.println("Your Shopping Cart Contains the following:");
+			for(int i=1;i<count;i++) {
+				System.out.println(i+". " + bookList[bookInCart[i]].title + " -- " + bookList[bookInCart[i]].author);
+			}
+			System.out.println("0. cancel");
+		}
+		System.out.println("Which number do you want to remove: ");
+		Scanner myScanner = new Scanner(System.in);
+		int remove = myScanner.nextInt();
+		if (remove == 0) {
+			return 0;
+		}
+		else if (remove > 0 && remove <= count) {
+			for(int i=1;i<count;i++) {
+				System.out.println("Selected: " + bookList[bookInCart[i]].title);
+			}	
+				System.out.println("Do you want to remove it: ");
+				String cancel = myScanner.next();
+				if (cancel.toLowerCase().equals("yes")) {
+					for(int i=1;i<count;i++) {
+						bookInCart[i+1]=bookInCart[i];
+						bookInCart[count] = 0;
+						count --;
+						if (isPhysical[i]==2) {
+							totalPrice -=50;
+						}
+						else if (isPhysical[i]==1) {
+							totalPrice -=8;
+						}
+					}
+					System.out.println("Item removes form Shopping Cart");
+				}
+				else if (cancel.toLowerCase().equals("no")) {
+					return 0;
+				}
+				else {
+					System.out.println("Sorry, that is an invalid option!");
+				}		
+		}
+		return 0;
+	}
+	
+	public static int checkout() {
+		System.out.println("You have purchased item to the total value of $"+ totalPrice);
+		System.out.println("Thanks for shopping with Daintree!");
+		bookInCart[count] = 0;
+		count = 0;
+		totalPrice = 0;
+		return 0;
+	}
+	
+	public static int listBook() {
+		System.out.println("The following titles are avaliable:");
+		for(int i = 0;i <= 4; i++) {
+			System.out.println((i+1)+". " + bookList[i].title + " -- " +bookList[i].author);
+		}
+		return 0;
+	}
+	
+}
